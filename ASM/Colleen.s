df3e12906606c6d6bdf6f52f4d8bf9e54ab890b8
mkneisl <189097252+mkneisl@users.noreply.github.com>
2025-05-12 21:43:39 +0200

    ASM Colleen working

---
+++ ASM/Colleen.s
@@ -0,0 +1,37 @@
+
+global main
+
+; This program will print its own source when run.
+
+%define ABB(str) "", 10,"global main", 10,"", 10,"; This program will print its own source when run.", 10,"", 10,"%define ABB(str) ", str, 10,"%defstr CODE ABB(str)", 10,"%strlen codeLen ABB(str)", 10,"", 10,"section .data", 10,"dataCode    db  ABB(CODE)", 10,"dataLen     equ $ - dataCode", 10,"", 10,"section .text", 10,"ft_putstr_fd:", 10,"    xor rdx, rdx", 10,"    mov rax, 1", 10," .loop:", 10,"    cmp BYTE[rdi + rdx], 0", 10,"    inc rdx", 10,"    jnz .loop", 10,"    syscall", 10,"    ret", 10,"", 10,"exit:", 10,"    mov rax, 60", 10,"    syscall", 10,"    int 3", 10,"", 10,"main:", 10,"    mov rdi, 1          ; Print source code & exit(0)", 10,"    lea rsi, dataCode", 10,"    mov rdx, dataLen", 10,"    mov rax, 1", 10,"    syscall", 10,"    xor rdi, rdi", 10,"    call exit", 10
+%defstr CODE ABB(str)
+%strlen codeLen ABB(str)
+
+section .data
+dataCode    db  ABB(CODE)
+dataLen     equ $ - dataCode
+
+section .text
+ft_putstr_fd:
+    xor rdx, rdx
+    mov rax, 1
+ .loop:
+    cmp BYTE[rdi + rdx], 0
+    inc rdx
+    jnz .loop
+    syscall
+    ret
+
+exit:
+    mov rax, 60
+    syscall
+    int 3
+
+main:
+    mov rdi, 1          ; Print source code & exit(0)
+    lea rsi, dataCode
+    mov rdx, dataLen
+    mov rax, 1
+    syscall
+    xor rdi, rdi
+    call exit
---
+++ ASM/Grace.s
@@ -0,0 +1,7 @@
+
+global main
+
+main:
+    mov rax, 60
+    syscall
+    int 3
