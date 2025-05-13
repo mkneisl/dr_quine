c98354239252da47167fafc34c2edcd1d4630b19
mkneisl <189097252+mkneisl@users.noreply.github.com>
2025-05-13 12:40:15 +0200

    Added tests & C Sully

--- C/Grace.c
+++ C/Grace.c
@@ -1,6 +1,6 @@
 #include <fcntl.h>
 #include <stdio.h>
-#define FT(x) int main() { FILE* f = fopen("Grace_kid.c", "w+"); if (f == 0) return 1; fputs("#include <fcntl.h>\n#include <stdio.h>\n#define FT(x) "x"\n#define STR(x) #x\n#define EXPAND(x) STR(x)\n\n/*\n This program will print its own source when run.\n*/\n\nFT(EXPAND(FT(x)))\n", f); fclose(f); return 0; }
+#define FT(x) int main() { FILE* f = fopen("Grace_kid.c", "w"); if (f == 0) return 1; fputs("#include <fcntl.h>\n#include <stdio.h>\n#define FT(x) "x"\n#define STR(x) #x\n#define EXPAND(x) STR(x)\n\n/*\n This program will print its own source when run.\n*/\n\nFT(EXPAND(FT(x)))\n", f); fclose(f); return 0; }
 #define STR(x) #x
 #define EXPAND(x) STR(x)
 
